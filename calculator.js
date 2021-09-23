554c042a7486626b1db789a2b06577840295c813
Pedro Nascimento <peedronasc@hotmail.com>
2021-09-23 00:00:28 -0300

    test correct

---
+++ calculator.js
@@ -0,0 +1,20 @@
+function soma(num1,num2) {
+    return num1+num2;
+}
+
+function subtracao(num1,num2) {
+    return num1-num2;
+}
+
+function multiplicacao(num1,num2) {
+    return num1*num2;
+}
+
+function divisao(num1,num2) {
+    return num1/num2;
+}
+
+exports.divisao = divisao;
+exports.soma = soma;
+exports.multiplicacao = multiplicacao;
+exports.subtracao = subtracao;
